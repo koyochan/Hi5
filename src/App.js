e054b1c7a8f353908a08f801331124342a7689f1
koyochan <121277745+koyochan@users.noreply.github.com>
2024-05-05 23:10:47 +0900

    git:

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import MySkillTreeComponent from './MySkillTreeComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hi_5 Skill Tree</h1>
+      </header>
+      <MySkillTreeComponent />
+      <MySkillTreeComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MySkillTreeComponent.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  SkillTreeGroup,
+  SkillTree,
+  SkillProvider,
+} from 'beautiful-skill-tree';
+
+const data_of_lego = [
+  {
+    id: '',
+    title: 'Hello World',
+    tooltip: {
+      content: 'このノードは最上位レベルで、アンロックされてクリック可能です。',
+    },
+    children: [
+      {
+        id: 'hello-sun',
+        title: 'Hello Sun',
+        tooltip: {
+          content: 'このノードは上位ノードの子で、親が選択状態でないとロックされます。',
+        },
+        children: [],
+      },
+      {
+        id: 'hello-stars',
+        title: 'Hello Stars',
+        tooltip: {
+          content: 'このツールは自動的にレイアウトを管理します。だからこれをBeautiful Skill Treeと呼びます。',
+        },
+        children: [],
+      },
+    ],
+  },
+];
+
+const MySkillTreeComponent = () => {
+  return (
+    <SkillProvider>
+      <SkillTreeGroup>
+        {({ skillCount }) => (
+          <SkillTree
+            treeId="first-tree"
+            title="Lego Education"
+            data={data_of_lego}
+            collapsible
+            description="Lego Education用のスキルツリー"
+          />
+        )}
+      </SkillTreeGroup>
+    </SkillProvider>
+  );
+};
+
+export default MySkillTreeComponent;
